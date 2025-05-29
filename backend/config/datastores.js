6dd8b015799f6ae41c7ef2129a50fbe77d128f48
ingenieriart3 <ingenieriart3@gmail.com>
2025-05-29 01:48:42 -0300

    deploy

--- backend/config/datastores.js
+++ backend/config/datastores.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+// require('dotenv').config();
+import { configDotenv } from 'dotenv';
 
 module.exports.datastores = {
   default: {
     adapter: 'sails-mongo',
-    url: process.env.MONGO_URL,
+    // url:'mongodb+srv://ingenieriart3:ItzeFBGGcCe8wA5g@growhardware.3oe3ko6.mongodb.net/growhardware?retryWrites=true&w=majority&appName=growhardware',//process.env.MONGO_URL,
+    url:process.env.MONGO_URL,
   },
 };
