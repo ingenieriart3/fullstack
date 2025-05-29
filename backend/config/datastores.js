73ee2c1c95bf22bef63754e23bbd6cfb50ee026b
ingenieriart3 <ingenieriart3@gmail.com>
2025-05-28 23:53:47 -0300

    deploy

--- backend/config/datastores.js
+++ backend/config/datastores.js
@@ -2,11 +2,11 @@ module.exports.datastores = {
   default: {
     adapter: 'sails-mongo',
     url: process.env.MONGO_URL,
-    ssl: true,
-    mongoClientOptions: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true
-    }
+    // ssl: true,
+    // mongoClientOptions: {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   ssl: true
+    // }
   },
 };
